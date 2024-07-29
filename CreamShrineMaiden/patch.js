854baa3a84a64c5205c708c34875f0849cbfd018
RogyWantsCoffee <Rogymaguito@outlook.com>
2024-07-28 23:45:59 -0500

    Miau

---
+++ CreamShrineMaiden/patch.js
@@ -0,0 +1,7 @@
+{
+    "id": "CreamShrineMaiden",
+    "servers": [
+        "https://mirrors.thpatch.net/RogyWantsCoffee/CreamShrineMaiden/"
+    ],
+    "CreamShrineMaiden": "Replaces Reimu Hakurei with Louie Cream (Rogy oc)"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+    "id": "RogyWantsCoffee//rogykoohii1",
+    "title": "Patches made a girl addicted to Coffee",
+    "contact": "Rogymaguito@outlook.com",
+    "servers": [
+        "https://mirrors.thpatch.net/RogyWantsCoffee/"
+    ],
+    "patches": {
+        "CreamShrineMaiden": "Replaces Reimu Hakurei with Louie Cream (Rogy oc)",
+    }
+}
