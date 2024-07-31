46df831223c1c6723da9d5dbaf49a5ca3958485d
RogyWantsCoffee <Rogymaguito@outlook.com>
2024-07-30 23:34:19 -0500

    Moon

--- repo.js
+++ repo.js
@@ -8,5 +8,6 @@
     "patches": {
         "CreamShrineMaiden": "Replaces Reimu Hakurei with Louie Cream (Rogy oc)",
         "KoreanBreakers": "WIP Splatic the bunny fighter wants to do justice to unacurrate Korean Food being served in cheap Korean restaurants! A mod for Uwabami Breakers.",
+        "LunarFestivalOfWishes": "It's september and the annual lunar festival of wishes is being the center of attention of youkai. Because of this, our heroes, Ruby, Rudy, Kidda, Collin and Alizee starts wandering around investigate and for the fun. (Th09)"
     }
 }
