5dace765fd509e9cbfc3b4bc7892ebce4728d56a
RogyWantsCoffee <Rogymaguito@outlook.com>
2024-07-30 23:36:24 -0500

    Kamizel

---
+++ LunarFestivalOfWishes/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "RogyWantsCoffee",
+	"servers": [
+		"https://mirrors.thpatch.net/RogyWantsCoffee/"
+	],
+	"title": "It's september and the annual lunar festival of wishes is being the center of attention of youkai. Because of this, our heroes, Ruby, Rudy, Kidda, Collin and Alizee starts wandering around investigate and for the fun. (Th09)"
+}
