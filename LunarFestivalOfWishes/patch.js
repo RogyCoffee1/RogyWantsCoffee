24b337bd1cae7d884909c1a0a81307ae53cf8483
RogyWantsCoffee <Rogymaguito@outlook.com>
2024-08-05 19:46:54 -0500

    kidda

--- LunarFestivalOfWishes/patch.js
+++ LunarFestivalOfWishes/patch.js
@@ -2,9 +2,9 @@
 	"dependencies": [
 		"nmlgc/base_tsa"
 	],
-	"id": "RogyWantsCoffee",
+	"id": "LunarFestivalOfWishes",
 	"servers": [
-		"https://mirrors.thpatch.net/RogyWantsCoffee/LunarFestivalOfWishes"
+		"https://mirrors.thpatch.net/RogyWantsCoffee/LunarFestivalOfWishes/"
 	],
-	"title": "It's september and the annual lunar festival of wishes is being the center of attention of youkai. Because of this, our heroes, Ruby, Rudy, Kidda, Collin and Alizee starts wandering around investigate and for the fun. (Th09)"
+		"title": "It's september and the annual lunar festival of wishes is being the center of attention of youkai. Because of this, our heroes, Ruby, Rudy, Kidda, Collin and Alizee starts wandering around investigate and for the fun. (Th09)"
 }
