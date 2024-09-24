ea5c86729653a164dffdda88d32a2a58819bf681
RogyWantsCoffee <Rogymaguito@outlook.com>
2024-09-23 19:42:57 -0500

    Fumouhou patch (replace characters with their fumo part!)

--- repo.js
+++ repo.js
@@ -8,6 +8,7 @@
     "patches": {
         "CreamShrineMaiden": "Replaces Reimu Hakurei with Louie Cream (Rogy oc)",
         "KoreanBreakers": "WIP Splatic the bunny fighter wants to do justice to unacurrate Korean Food being served in cheap Korean restaurants! A mod for Uwabami Breakers.",
-        "LunarFestivalOfWishes": "It's september and the annual lunar festival of wishes is being the center of attention of youkai. Because of this, our heroes, Ruby, Rudy, Kidda, Collin and Alizee starts wandering around investigate and for the fun. (Th09)"
-    }
+        "LunarFestivalOfWishes": "It's september and the annual lunar festival of wishes is being the center of attention of youkai. Because of this, our heroes, Ruby, Rudy, Kidda, Collin and Alizee starts wandering around investigate and for the fun. (Th09)",
+		"Fumouhou": "replace characters sprites, portraits and menus with their fumo counterpart!! (Also, i know the name sucks)"
+  }
 }
