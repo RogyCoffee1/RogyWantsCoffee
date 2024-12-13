afbf6780c894537121889c56b06a0c8117b34b07
RogyWantsCoffee <Rogymaguito@outlook.com>
2024-12-13 04:27:17 -0500

    Lunar Festival DLC

---
+++ LunarFestivalDLC/patch.js
@@ -0,0 +1,16 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+        "thpatch/lunarFestivalOfWishes"
+	],
+	"fonts": {
+		"mangat.ttf": true
+	},
+	"id": "LunarFestivalDLC",
+	"servers": [
+		"https://mirrors.thpatch.net/RogyWantsCoffee/LunarFestivalDLC"
+	],
+		"title": "(Th09) Adds Merry Noel and Zara Landmorri."
+}
