0e233b525e9d707863eb8b62d3c4d77b46c4bf4f
RogyCoffee <rogymaguito@outlook.com>
2025-01-06 19:23:59 -0500

    Update repo.js

--- repo.js
+++ repo.js
@@ -1,7 +1,7 @@
 {
     "id": "RogyWantsCoffee - rogykoohii1",
-    "title": "Patches made a girl addicted to Coffee",
-    "contact": "Rogymaguito@outlook.com / rogykoohii1 on Discord",
+    "title": "Patches made by a girl addicted to Coffee.",
+    "contact": "rogykoohii1 on Discord",
     "servers": [
         "https://mirrors.thpatch.net/RogyWantsCoffee/"
     ],
