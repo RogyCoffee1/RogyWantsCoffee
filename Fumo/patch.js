318a01fa231c704e6e1c211eea1cc4682d768467
RogyCoffee <rogymaguito@outlook.com>
2025-07-29 05:35:51 -0500

    Fumo

---
+++ Fumo/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "Fumo",
+	"servers": [
+		"https://mirrors.thpatch.net/RogyWantsCoffee/Fumo/"
+	],
+	"title": "(Th16) A silly patch that replaces some characters with their fumo counterpart."
+}
